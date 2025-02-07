d1cb31518279a7f224cd61d070c62e7ca9834ffb
Aqsaali7 <aaqsa5411@gmail.com>
2025-02-07 18:58:05 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */module , exports: {
-    images: {
-      domains: ['example.com'], // Allow external images from specific domains
-    },
-  },
-
-};
+  
+}
+  /* config options here */
 
 export default nextConfig;
